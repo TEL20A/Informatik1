ef59d57ca90b5746f07215010fac64ec449381eb
toj11001 <tobias.joschko@gmx.de>
2021-02-19 11:35:08 +0100

    DamenProblem

--- Vorlesungsmaterial/2021-02-18-lab/DamenProblem.cpp
+++ Vorlesungsmaterial/2021-02-18-lab/DamenProblem.cpp
@@ -4,69 +4,40 @@
 
 using namespace std;
 
-bool freie_spalte(int neu_reihe, int neue_spalte, vector<int> damen_spalten) {
-   for(int reihe=0; reihe < neu_reihe; reihe++) {
-      if (damen_spalten[reihe] == neue_spalte ||
-          damen_spalten[reihe] + reihe == neu_reihe + neue_spalte ||
-          damen_spalten[reihe] - reihe == neue_spalte - neu_reihe
-         ) {
-            return false;
-         }
-   }
-   return true;
-}
-
-vector<vector<int>> platziere_dame(int neue_reihe, int max_spalten, vector<vector<int>> damen_lsg) {
-   vector<vector<int>> neu_lsg;
-   for(auto damen_spalten:damen_lsg) {
-      // laufe durch alle spalten der Reihe
-      for(int spalte=0; spalte < max_spalten; spalte++){
-         if(freie_spalte(neue_reihe,spalte, damen_spalten)) {
-            vector<int> temp = damen_spalten;
-            temp.push_back(spalte);
-            neu_lsg.push_back(temp);
-         }
-      }
-   }
-   return neu_lsg;
-}
-
-// durchlaufe Reihen
-vector<vector<int>> damen_problen(int reihen, int spalten) {
-   if(reihen == 0) {
-      vector<vector<int>> damen = {{}};
-      return damen;
-   }
-   else {
-      return platziere_dame(reihen-1, spalten, damen_problen(reihen-1,spalten));
-   }
-}
+// Globale Variablen
 
 
-void print_board(vector<vector<int>> &damen) {
-   for(auto d:damen) {
-      cout << "\nLoesung: \n";
-      for(auto d2:d){
-         // int breite = d2.size();
-         int dame_pos = d2;
-         for(int i=0; i < d.size(); i++) {
-            if(i==dame_pos)
-               cout << "D";
-            else
-               cout << "-";
-         }
-         cout << endl;
-      }
-   }
-}
+// Prototypen
+bool freies_feld(int neu_reihe, int neue_spalte, /* spielfeld */);
+<typ> platziere_dame();
+<typ> damen_problen();
+void print_board(spielfeld);
+
 int main(int argc, char *argv[]) {
-   /*
-   if(argc == 2) {
-      int n_damen = (int) *argv[1];
-      cout << n_damen;
-   }
-   */
-   vector<vector<int>> damen = damen_problen(6,6);
-   print_board(damen);
+
    return EXIT_SUCCESS;
+}
+
+// Diese Funktion soll prüfen ob ein bestimmtes Feld frei ist
+bool freies_feld(int neu_reihe, int neue_spalte, /* spielfeld */) {
+
+}
+
+// Diese Funktion soll eine Dame in einer Reihe platzieren
+<typ> platziere_dame() {
+
+
+}
+
+// Die rekursive Funktion die ein durch die Reihen des Feldes läuft
+<typ> damen_problen() {
+   // Abbruch Bedingung
+
+   // Rekursion
+
+}
+
+// Ausgabe eines Spielfelds
+void print_board(spielfeld){
+
 }
